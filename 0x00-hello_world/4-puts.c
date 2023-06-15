1992b499dcfec06c62c4d489b9e30efaf709c536
AnasAL21 <anasaitlhaj2108@gmail.com>
2023-06-15 09:42:54 -0700

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+/**
+ * main- a C program that prints a new line using puts
+ * Return: 0 (Success)
+ */
 int main(void)
 {  puts("\"Programming is like building a multilingual puzzle");
 return (0);  }
