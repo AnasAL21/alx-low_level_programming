aa38a86399d0e1d367801e723d50f935a450160a
AnasAL21 <anasaitlhaj2108@gmail.com>
2023-06-15 10:00:53 -0700

    update

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,8 @@
-
+#include <stdio.h>
+/**
+ * main - a C program that prints a new line using printf
+ * Return: 0 (Success)
+ */
+int main(void)
+{  printf(" with proper grammar, but the outcome is a piece of art,\n");
+return (0); }
