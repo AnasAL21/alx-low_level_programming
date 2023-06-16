31e5e4ad6c12a4ab573af55d4a99828d8008a306
AnasAL21 <anasaitlhaj2108@gmail.com>
2023-06-16 06:59:08 -0700

    update

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - The main function
+ *
+ * program that prints the alphabet in lowercase followed by a new line
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char alp[26] = "abcdefghijklmnepqrstuvwxyz";
+	int i;
+	/*******/
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
+}
