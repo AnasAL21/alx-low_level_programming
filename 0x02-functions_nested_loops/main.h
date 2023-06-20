f7174d4db574e184503ee2b7cce66dd33b2f1c93
AnasAL21 <anasaitlhaj2108@gmail.com>
2023-06-20 09:55:57 -0700

    update

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,19 +1,27 @@
-#ifndef main_h
-#define main_h
+#include "main.h"
 
-int _putchar(char);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
-void jack_bauer(void);
-void times_table(void);
-int add(int, int);
-void print_to_98(int n);
-void print_times_table(int n);
-
-#endif
-
+/**
+ * print_sign - prints the sign of a number
+ * @n: the int to check
+ * Return: 1 and prints + if n is greater than zero
+ * 0 and prints 0 if n is zero
+ * -1 and prints - if n is less than zero
+ */
+int print_sign(int n)
+{
+        if (n > 0)
+        {
+                _putchar('+');
+                return (1);
+        } 
+        else if (n == 0)
+        {
+                _putchar(48);
+                return (0);
+        }
+        else if (n < 0)
+        {
+                _putchar('-');
+                return (-1);
+        }
+}
