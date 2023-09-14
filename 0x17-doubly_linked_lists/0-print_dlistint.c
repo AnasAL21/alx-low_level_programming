85f72e33fe6b3e2883e22c0055602e4a35e63a00
AnasAL21 <anasaitlhaj2108@gmail.com>
2023-09-14 07:17:59 -0700

    update

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,15 +1,13 @@
 #include "lists.h"
-
 /**
  *print_distint - prints doubly-linked list
  *@h: adress of head node
  *Return: size od list
  */
-
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t i = 0;
-	
+	/*****/
 	while (h)
 	{
 		print("%d\n", h->n);
